b34a6467695f507cc36f6a8345e55d0208fbf246
Orlando Monteverde <orlmicron@gmail.com>
2018-08-15 16:52:58 -0400

    Tipos añadidos

--- modules/extract.py
+++ modules/extract.py
@@ -3,6 +3,21 @@ from bs4 import BeautifulSoup
 
 
 URL = "http://www.wordreference.com/definicion/"
+RESPUESTAS = {
+    "m": "Nombre Masculino",
+    "f": "Nombre Femenino",
+    "v": "Verbo",
+    "tr": "Verbo",
+    "suf": "Sufijo",
+    "interj": "interjección",
+    "art": "Articulo",
+    "intr": "Verbo",
+    "adj": "Adjetivo",
+    "conj": "Conjunción",
+    "pron": "Pronombre",
+    "contr": "Contracción de la preposición + artículo",
+    "adv": "Adverbio",
+}
 
 
 def extract(palabra: str):
@@ -10,8 +25,10 @@ def extract(palabra: str):
 
     if request.status_code == 200:
         soup = BeautifulSoup(request.text, "html.parser")
-        definition = soup.find("ol", {"class": "entry"}).getText()
-        tipo = definition.split(". ")[0]
-        return tipo
+        definition = soup.find("ol", {"class": "entry"})
 
-    return "Not found"
+        if definition:
+            tipo = definition.getText().split(". ")[0]
+            return RESPUESTAS.get(tipo, f"Tipo no asignado: {tipo}")
+
+    return "No encontrada"
