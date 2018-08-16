35bc7f95630cbaddbc88dafffff5cbc3d36dd1d6
javier <33096723+javi20gu@users.noreply.github.com>
2018-08-16 14:17:58 +0200

    Mejoras de código

---
+++ modules/dicc.py
@@ -0,0 +1,17 @@
+
+
+ABREVIACIONES = {
+    "adv": "Adverbio",
+    "contr": "Contracción de la preposición + artículo",
+    "pron": "Pronombre",
+    "conj": "Conjunción",
+    "adj": "Adjetivo",
+    "intr": "Verbo",
+    "art": "Articulo",
+    "interj": "interjección",
+    "m": "Nombre Masculino",
+    "f": "Nombre Femenino",
+    "v": "Verbo",
+    "tr": "Verbo",
+    "suf": "Sufijo"
+}
--- modules/extract.py
+++ modules/extract.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from .dicc import ABREVIACIONES
 
 
 def extract(palabra: str):
@@ -17,32 +18,8 @@ def extract(palabra: str):
                     palabras = ""
                 else:
                     palabras += letra
-            if tipo[0] == "m":
-                return "Nombre Masculino"
-            elif tipo[0] == "f":
-                return "Nombre Femenino"
-            elif tipo[0] == "v":
-                return "Verbo"
-            elif tipo[0] == "tr":
-                return "Verbo"
-            elif tipo[0] == "suf":
-                return "Sufijo"
-            elif tipo[0] == "interj":
-                return "interjección"
-            elif tipo[0] == "art":
-                return "Articulo"
-            elif tipo[0] == "intr":
-                return "Verbo"
-            elif tipo[0] == "adj":
-                return "Adjetivo"
-            elif tipo[0] == "conj":
-                return "Conjunción"
-            elif tipo[0] == "pron":
-                return "Pronombre"
-            elif tipo[0] == "contr":
-                return "Contracción de la preposición + artículo"
-            elif tipo[0] == "adv":
-                return "Adverbio"
+            if tipo[0] != "":
+                return ABREVIACIONES[tipo[0]]
             else:
                 return "Tipo no asignado: {}".format(tipo[0])
         else:
