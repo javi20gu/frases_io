68e5a8cd6d60a77aca7473759d230de1a60cda77
Javier <javierhidalgo_c@hotmail.com>
2018-08-18 17:07:24 +0200

    Mejoras de Codigo

--- app.py
+++ app.py
@@ -1,14 +1,24 @@
-import sys
+from sys import argv
 
 from frasesio import frasesio
 from web import app
 
 
 def main():
-    if "server" in sys.argv or "-s" in sys.argv:
+    if "server" in argv or "-s" in argv:
         app.run()
-    else:
+    elif "help" in argv or "-h" in argv: 
+        print("Comandos Utililes:\
+        \nserver o -s: Inicia la app en las web.\
+        \nhelp o -h: Te dice los comandos utiles.\
+        \nconsole o -c: Inicia en la consola.\
+        ")
+    elif "console" in argv or "-c" in argv:
         frasesio()
+    else:
+       print(
+           "Error: Comando incorrecto,"
+            " consulte el comando help o -h.")
 
 
 if __name__ == '__main__':
